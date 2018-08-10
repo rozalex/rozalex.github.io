deb6a19b1a843d7e27043b175eef8f0ffcaf7de7
Alex Rozhavsky <rozalex@gmail.com>
2018-08-10 23:20:36 +0300

    code snippets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Button from './components/button/Button';
 import Switch from './components/switch/Switch';
+import { Icon } from 'react-icons-kit'
+import { fileCodeO } from 'react-icons-kit/fa/fileCodeO'
 
 import './style/App.css';
 import './style/App.rtl.css';
@@ -11,14 +13,26 @@ class App extends Component {
     super();
 
     this.handleSwitch = this.handleSwitch.bind(this);
+    this.showSlider = this.showSlider.bind(this);
+    this.hideSlider = this.hideSlider.bind(this);
+
     this.state = {
-      direction: "ltr"
+      direction: "ltr",
+      buttonsSlider: false
     }
   }
 
   handleSwitch() {
     const nextDirection = this.state.direction === 'ltr' ? 'rtl' : 'ltr';
     this.setState({direction: nextDirection});
+  }
+
+  showSlider() {
+    this.setState({buttonsSlider: true});
+  }
+
+  hideSlider() {
+    this.setState({buttonsSlider: false});
   }
 
   render() {
@@ -30,7 +44,7 @@ class App extends Component {
       url: "https://www.w3schools.com/howto/img_avatar.png"
     }
 
-    const { direction } = this.state;
+    const { direction, buttonsSlider } = this.state;
     return (
       <div className={`App ${this.state.direction}`}>
           <Header {...data} />
@@ -42,7 +56,18 @@ class App extends Component {
               <Switch buttonClass="purple" checked={direction === 'ltr'} handleSwitch={this.handleSwitch} /> 
             </section>
             <section>
-              <h2>Buttons</h2>
+              <div className={ buttonsSlider ? "code-snippet-on" : "code-snippet-off" }>
+                <div className="slide-menu" onClick={this.hideSlider}>
+                  <Icon icon={fileCodeO} />
+                </div>
+                <div className="code">
+                  <div>{`<Button buttonClass='your-class' title={your-title} />`}</div>
+                </div>
+              </div>
+              <div className="slide-menu" onClick={this.showSlider}>
+                <Icon icon={fileCodeO} />
+              </div>
+              <div className="section-title">Buttons</div>
               <Button buttonClass="blue" title={data.hebrewTitle } />
               <Button buttonClass="green" title={data.title } />
               <Button buttonClass="purple" title={data.longTitle } />
